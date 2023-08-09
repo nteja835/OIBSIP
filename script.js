f89541e44989afe0cbe400f42b7ec9b9fe94cced
nteja835 <113295269+nteja835@users.noreply.github.com>
2023-08-09 17:08:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function convertCelsiusToFahrenheit() {
+    var celsius = parseFloat(document.getElementById("celsius").value);
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("result").innerHTML = celsius + "°C is " + fahrenheit.toFixed(2) + "°F";
+}
